c6c667a0f36781b150822db9e4d0f10b5c3822c6
sktrinh12 <strkinh12@gmail.com>
2025-11-25 17:16:26 -0500

    US376|added oidc provider, chromas3access policy and role, ebs csi driver, aws alb policies and roles

--- cluster.tf
+++ cluster.tf
@@ -56,6 +56,137 @@ resource "aws_iam_role" "nodes_eks" {
 POLICY
 }
 
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [
+    "9e99a48a9960b14926bb7f3b02e22da0afd10a3d"
+  ]
+}
+
+resource "aws_iam_role" "chromadb_s3_access_role" {
+  name = "ChromaDBS3AccessRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:app:chroma-s3-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "chromadb_s3_access" {
+  statement {
+    sid    = "ListBucket"
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::preludetx-strinh",
+    ]
+  }
+
+  statement {
+    sid    = "ObjectAccess"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::preludetx-strinh/*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "chromadb_s3_access_inline" {
+  name   = "ChromaDBS3AccessPolicy"
+  role   = aws_iam_role.chromadb_s3_access_role.id
+  policy = data.aws_iam_policy_document.chromadb_s3_access.json
+}
+
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "AmazonEKS_EBS_CSI_DriverRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "IAM policy for AWS Load Balancer Controller"
+
+  policy = file("${path.module}/aws-alb-iam-policy.json")
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  name = "AmazonEKS_AWS_LoadBalancer_Controller_Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
 resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy_eks" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.nodes_eks.name
